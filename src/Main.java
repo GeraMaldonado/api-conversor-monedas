d2682b65b7aeb2481bb9e86d1158c8937b8df15b
Gerardo Maldonado Félix <gmaldonadofelix@gmail.com>
2024-05-16 16:33:42 -0600

    Primeros pasos con la consulta

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,36 @@
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner escaner = new Scanner(System.in);
+        System.out.println("Escribe lo que quieras, como quiera no me importa: ");
+        String busqueda = escaner.nextLine();
+        String url = "https://v6.exchangerate-api.com/v6/2d84d3354cd4b4d1ee5fd297/latest/"+busqueda.toUpperCase();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        String json = response.body();
+        System.out.println(json);
+//        System.out.println(json);
+//        Gson gson = new Gson();
+//                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+//                .create();
+//        System.out.println(gson.fromJson(json));
     }
 }
